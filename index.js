d39ceb157962bc3e0e7255eadb3a5fb5f46a87db
darren9897 <darrenkoomson17@gmail.com>
2020-06-19 15:23:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, str){
+    let birthdayCards = []                                               
+
+    for(let i = 0; i < arr.length ; i++){                             
+        birthdayCards.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);   
+    }
+
+    return birthdayCards;
+};
+
+function countDown(num){
+    for(let i = 0; num > i; num--){
+        console.log(num);
+    }
+    return console.log(num);
+}
